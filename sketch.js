762ae185da0e34819536f4e01e26266f41f6c080
pinocoder <41514345+pinocoder@users.noreply.github.com>
2018-10-03 00:57:05 +0200

    Second delivery

--- sketch.js
+++ sketch.js
@@ -1,11 +1,34 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  angleMode(DEGREES);
+
+  frameRate(12);
 }
 
 function draw() {
-  // put drawing code here
+
+  //linee
+  push();
+  noFill();
+  stroke(0);
+  translate(width/2, height/2);
+  //rotate(frameCount*9);
+  stroke(0);
+  line(200,0,cos(frameCount*3)*200,sin(frameCount*3)*200);
+  pop();
+
+  //pallina
+  translate(width/2,height/2);
+  rotate(frameCount*3);
+  stroke('#181818');
+  ellipse(210,0,10,10);
+
+
+
+  // Stops the draw function if a certain condition avaluates true
+  if (frameCount == 120) {
+    noLoop();
+  }
+
 }
